cddfa50bfc9f373ecfa7371032e384652cd6f4d8
getpythoncode-help <82193102+getpythoncode-help@users.noreply.github.com>
2021-04-15 13:23:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("appnavbar").style.width = "296px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("appnavbar").style.width = "0";
+  document.body.style.backgroundColor = "#fff";
+}
