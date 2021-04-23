2d4d06e8dbeaed5abb95979c7567e06d198aaa10
getpythoncode-help <82193102+getpythoncode-help@users.noreply.github.com>
2021-04-23 13:46:09 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 function openNav() {
   document.getElementById("appnavbar").style.width = "296px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.body.style.backgroundColor = "#c8c8c8";
 }
 
 function closeNav() {
   document.getElementById("appnavbar").style.width = "0";
   document.body.style.backgroundColor = "#fff";
 }
+
+var i = 0;
+var txt = 'Get Python Code';
+var speed = 130;
+
+function text(){
+  if(i < txt.length){
+  document.getElementById("typing")
+  .innerHTML += txt.charAt(i);
+  i++;
+  setTimeout(text, speed)
+}
+}
+
+console.log("localhost:8888");
