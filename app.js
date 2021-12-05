d83e2526beac8c9c89d85aab9e0dbdb5c2a2498a
getpythoncode-help <82193102+getpythoncode-help@users.noreply.github.com>
2021-12-05 21:47:17 +0530

    Revert "poswerwash"
    This reverts commit 8555114409a6a10d7ae4b1d8e8ae2b7aa3d4b692.

---
+++ app.js
@@ -0,0 +1,20 @@
+function openNav() {document.getElementById("get-nav").style.width = "296px";document.body.style.backgroundColor = "#c8c8c8";}
+function closeNav() {document.getElementById("get-nav").style.width = "0";document.body.style.backgroundColor = "#fff";}
+function getpythoncode(){window.location.href="index.html";}
+
+const api = 'AIzaSyCVF-L8qPeRAmIbx0nnte72uDdXFNxXWlc';
+const ChannelID = 'UCEBt6_mbQsaAvSbsv2xwxiQ';
+const subscriberCount = document.getElementById("subscriberCount");
+
+    let getSubscribers = () => {
+        fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${ChannelID}&key=${api}`)
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            console.log(data)
+            var count = data["items"][0].statistics.subscriberCount;
+            subscriberCount.innerHTML = count/1000 + "K subscribers"
+        })
+    }
+    getSubscribers();
