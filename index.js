ff77badc979159fa94a522ac894316fdcd83ac31
getpythoncode-help <82193102+getpythoncode-help@users.noreply.github.com>
2024-02-26 23:29:55 +0530

    Revert "Add files via upload"
    This reverts commit 77c0130fa042b4a8dbea2a61527d84cc2a28fdd3.

--- index.js
+++
@@ -1,9 +0,0 @@
-const header = document.querySelector("header");
-fetch("/header.html")
-    .then(response => response.text())
-    .then(data => {
-        header.innerHTML = data
-        const parser = new DOMParser()
-        const doc = parser.parseFromString(data, 'text/html')
-        eval(doc.querySelector('script').textContent)
-    })
